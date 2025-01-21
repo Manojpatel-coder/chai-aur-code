62244d2b76bdd2a28b09013325ac7762c6cab94c
Manoj Patel <lifeatlnct2022@gmail.com>
2025-01-21 07:05:12 +0530

    react with vite

---
+++ 01_basicreact/src/App.jsx
@@ -0,0 +1,14 @@
+//if you are running with vite file must be named jdx as chai is named
+import Chai from "./Chai"
+function App() {
+  
+
+  return (
+<>
+<h1>Chai aur react With vite | manoj</h1>
+<Chai/>
+</>
+  )
+}
+
+export default App
